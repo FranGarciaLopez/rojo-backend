d8e9ad231e0763f9b1542f515017932667b044d0
belen579 <belenbastosdavila@hotmail.com>
2024-10-09 23:46:57 +0200

    create collection city link with user attribute

--- models/City.js
+++ models/City.js
@@ -4,11 +4,11 @@ const Schema = mongoose.Schema;
 
 const CitySchema = new Schema({
     name: { type: String, required: true }, 
-    state: { type: String, required: true },
-    country: { type: String, required: true }, 
+    state: { type: String },
+    country: { type: String}, 
 
 
 
 });
 
-module.exports = mongoose.model("City", citySchema);
+module.exports = mongoose.model("City", CitySchema);
