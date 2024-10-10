23cadcebb83f0d274b0743800690f803cdd3daef
Fran <frangalo34@gmail.com>
2024-10-10 11:51:04 +0200

    change midleware location
    - update error message handling
    - delete console log

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -1,22 +1,27 @@
 const jwt = require("jsonwebtoken");
 
 const authMiddlewares = {
+
   validateToken: (req, res, next) => {
+    
     if (!req.headers.authorization) {
-      return res.status(401).send("You are not authorized");
+      return res.status(401).json({ message: "You are not authorized" });
     }
+
     try {
+
       const token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.SECRET);
-      console.log("decoded:", decoded);
-      //decoded: { id: 1, role: 'user', iat: 1631080000 }
+
       req.user = { ...decoded };
       next();
+
     } catch (err) {
       console.log("Error validating token", err);
       return res.status(401).send("You are not authorized");
     }
   },
+
   isAdmin: (req, res, next) => {
     if (req.user.role !== "admin") {
       return res
