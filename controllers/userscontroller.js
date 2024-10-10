d98d1c5e866db68fa100e3efd59b8be608857a16
Fran <frangalo34@gmail.com>
2024-10-10 11:59:28 +0200

    merge duplicated user controller and update logic
    - get only one user instead every one in the db
    - add user exists management error
    - return alternative format for error messages.
    - delete unnecessary lines
    - delete unnecessary imports
    - format document

--- controllers/userscontroller.js
+++ controllers/userscontroller.js
@@ -1,91 +1,72 @@
-
-
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
-const middlewares = require('../middlewares');
-const dotenv = require("dotenv");
-const City = require('../models/City'); 
+// const City = require('../models/City');
 
+const userController = {
 
-async function userRegister(req, res) {
-    let cityDocument = await City.findOne({ name: req.body.city  });
-    if (!cityDocument) {
-        cityDocument = new City({ name: req.body.city });
-        await cityDocument.save();
-      }
-     
+  async userRegister(req, res) {
+    const { firstname, lastname, email, password, city, dateOfBirth } = req.body;
 
-        
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(409).json({ message: 'User already exists' });
+    }
+
     const newUser = new User({
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        email: req.body.email,
-        password: req.body.password,
-        city: cityDocument._id,
-        dateOfBirth: new Date(req.body.dateOfBirth),
-        isAdministrator: req.body.isAdministrator,
+      firstname,
+      lastname,
+      email,
+      password,
+      city,
+      dateOfBirth,
+      isAdmin: false,
+      organizedEvents: 0,
+      joinedEvents: 0,
+      modifiedAt: new Date().toISOString(),
+      createdAt: new Date().toISOString(),
+      deletedAt: null,
     });
 
-  try {
+    await newUser.save();
+    res.status(201).json({
+      status: 'success',
+      message: 'User created successfully'
+    });
+  },
 
-
-    
-   
-    const savedUser = await newUser.save(); 
-    res.status(201).json(savedUser); 
-
-     
-  } catch (error) {
-      console.error("Error creating user:", error);
-      res.status(500).json({ message: "Error creating user:" }); 
-  }
-}
-
-async function userLogin (req,res){
+  async userLogin(req, res) {
     const { email, password } = req.body;
-        const user = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-        console.log("usuario" ,user);
-
-  if (user && user.password === password) {
-
-  
-    const token = jwt.sign(
-      { id: user.email, role: user.firstname },
-     process.env.SECRET
-    );
-    return res.status(201).send({ token });
-  }
-  return res.status(401).send("Username or password is not correct");
-
-
-}
-
-
-async function getAllUsers(req, res) {
-    try {
-        const users = await User.find(); 
-        return res.status(200).json(users); 
-    } catch (error) {
-        console.error("Error getting users:", error);
-        return res.status(500).send("Error getting users");
+    if (user && user.password === password) {
+      const token = jwt.sign(
+        { email: user.email, firstname: user.firstname, isAdministrator: user.isAdministrator },
+        process.env.SECRET
+      );
+      return res.status(200).send({ token });
     }
-}
+    return res.status(401).json({ message: "Invalid credentials" });
+  },
 
+  async getUser(req, res) {
+    try {
+      const user = await User.findOne({ firstname: req.user.firstname, email: req.user.email })
+        .populate('city')
+        .populate('events');
 
+      res.status(200).json({
+        status: "success",
+        data: {
+          user,
+        },
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: "error",
+        message: "Error fetching users"
+      });
+    }
+  }
+};
 
-
-
-
-
-
-
-
- module.exports = { userRegister , userLogin, getAllUsers};
-
-
-
-
-
-
-  
+module.exports = userController;
