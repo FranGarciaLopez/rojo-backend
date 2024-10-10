63cb08cbebe9fb8f3858fa7cb5bb97ff15a0d64d
Fran <frangalo34@gmail.com>
2024-10-10 11:56:35 +0200

    change index file.
    - delete un-necessary imports
    - properly format the document
    - delete commented functions and random secret generator.
    - delete unnecessary console logs.

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 // IMPORTING ----------------------
 const express = require('express');
-const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
-const users = require("./database/users");
-const authMiddlewares = require("./middlewares");
-const usersRouter= require("./routes/usersRouter");
+
+const usersRouter = require("./routes/usersRouter");
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-
-// basic cors requests from the front in 5173
-
+// CORS -----------------------------
 app.use(cors({
   origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-  credentials: true, 
+  credentials: true,
 }));
+
+const port = 3000;
 
 // DB SETTING --------------------
 dotenv.config();
 
 const mongoose = require("mongoose");
 const encodePassword = encodeURIComponent(process.env.DB_PASSWORD);
+
 const mongoDB =
   "mongodb+srv://" +
   process.env.DB_USER +
@@ -34,62 +33,22 @@ const mongoDB =
   "/" +
   process.env.DB_NAME +
   "?retryWrites=true&w=majority";
+
 async function main() {
   try {
     await mongoose.connect(mongoDB);
-      console.log("Conexión a MongoDB exitosa");
   } catch (err) {
-    console.log("Error conectando a MongoDB:", err);
+    console.log(err);
   }
 }
 main().catch((err) => console.log(err));
 
-// ROUTES -----------------------------
-
-
-app.use(express.json());
-
-
+// ROUTES -------------------------
 app.use('/', usersRouter);
 
-const crypto = require('crypto');
-
-function generateSecret(length = 32) {
-    return crypto.randomBytes(length).toString('hex');
-}
-
-console.log(generateSecret(16));
-
-
-/*
-  app.get("/", (req, res) => {
-    res.send("Hello World 2!");
-  });
-
-app.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  const user = users.find((userDB) => userDB.username === username);
-  if (user && user.password === password) {
-    const token = jwt.sign(
-      { id: user.id, role: user.role },
-      process.env.SECRET
-    );
-    return res.status(201).send({ token });
-  }
-  return res.status(401).send("Username or password is not correct");
-});
-
-  app.get("/user", authMiddlewares.validateToken, (req, res) => {
-    const user = users.find((userDB) => userDB.id === req.user.id);
-    res.status(200).send(user);
-  });
-
-app.get("/secret", (req, res) => {
-  res.send(require("crypto").randomBytes(32).toString("hex"));
-});*/
-
-// Listening ---------------------------------------------------
+// SERVER -------------------------
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
 module.exports = { app };
