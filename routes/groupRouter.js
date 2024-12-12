69f5c1725a8ea06cf5ff44a383fa0968def39589
Fran <frangalo34@gmail.com>
2024-12-12 16:09:59 +0100

    Refactor group controller for better performance, error handling, and code maintainability.

--- routes/groupRouter.js
+++ routes/groupRouter.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const groupController = require('../controllers/groupController');
-
 const groupRouter = express.Router();
+const { validateToken } = require('../middlewares/middlewares');
+
 
 //groupRouter.get('/show', groupController.show);
 groupRouter.post('/create', groupController.create);
-groupRouter.post('/eraseall', groupController.eraseall);
-groupRouter.post('/findgroup', groupController.findgroup);
-groupRouter.post('/showall', groupController.showall);
+/* groupRouter.post('/eraseall', groupController.eraseall); */
+/* groupRouter.post('/findgroup', groupController.findgroup); */
+/* groupRouter.post('/showall', groupController.showall); */
+// groupRouter.js
+groupRouter.post('/:groupId/message', validateToken, groupController.sendMessage);
+groupRouter.get('/:groupId/messages', validateToken, groupController.getMessages);
 
 module.exports = groupRouter;
