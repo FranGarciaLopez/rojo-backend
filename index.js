4add7774775af61396579fb12ad1cded8de511d0
Fran <frangalo34@gmail.com>
2024-12-27 17:49:22 +0100

    add chors for the deployed frontend

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(express.json());
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: ['http://localhost:5173','https://rojo-frontend.onrender.com'],
         methods: ['GET', 'POST'],
     }
 });
@@ -119,7 +119,7 @@ cloudinary.config({
 });
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://rojo-frontend.onrender.com'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true,
 }));
