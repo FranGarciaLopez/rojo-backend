642f4ca2d3ed1b8bcdaab6d0ab290fefea44c7e8
Fran <frangalo34@gmail.com>
2025-01-05 13:47:39 +0100

    add get blog details function

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -14,17 +14,15 @@ const multer = require('multer');
 const upload = multer({ storage: multer.memoryStorage() });
 
 
-const blogController ={
+const blogController = {
 
+    async blogRegister(req, res) {
+        const { title, description, user, photo } = req.body;
 
-    async blogRegister (req, res){
-        const {title, description, user, photo}= req.body;
-
-        let userDocument = await User.findOne({name:user._id});
-        if(!userDocument){
-            return res.status(400).json({message: 'User does not exist'});
+        let userDocument = await User.findOne({ name: user._id });
+        if (!userDocument) {
+            return res.status(400).json({ message: 'User does not exist' });
         }
-
 
         const newBlog = new Blog({
             title,
@@ -41,30 +39,49 @@ const blogController ={
                 message: 'Blog create sucessfully'
             }
         );
-
-
-
-
     },
 
-    async getBlogs (req, res){
+    async getBlogs(req, res) {
         try {
             const blogs = await Blog.find()
-                  .populate('user', 'avatar firstname lastname '); // Populate the correct paths
+                .populate('user', 'avatar firstname lastname '); // Populate the correct paths
 
             res.status(200).json({
-                   status: "success",
-                   blogs
+                status: "success",
+                blogs
             });
-     } catch (error) {
+        } catch (error) {
             return res.status(500).json({
-                   status: "error",
-                   message: "Error fetching bkigs"
+                status: "error",
+                message: "Error fetching bkigs"
             });
-     }
-},
+        }
+    },
 
-
-    };
+    async getBlog(req, res) {
+        try {
+            const blog = await Blog.findById(req.params.id)
+                .populate('user', 'avatar firstname lastname');
     
-    module.exports = blogController;
+            if (!blog) {
+                return res.status(404).json({
+                    status: "error",
+                    message: "Blog not found",
+                });
+            }
+    
+            res.status(200).json({
+                status: "success",
+                blog,
+            });
+        } catch (error) {
+            return res.status(500).json({
+                status: "error",
+                message: "Error fetching blog",
+            });
+        }
+    }
+    
+};
+
+module.exports = blogController;
