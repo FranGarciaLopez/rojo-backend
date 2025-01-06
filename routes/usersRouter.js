d9207c51c3f6a0a8ed4ee228a28cc173af9560b0
Fran <frangalo34@gmail.com>
2025-01-06 15:12:10 +0100

    add delete profile and delete user by id

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const userController = require('../controllers/userscontroller');
-const { validateToken } = require('../middlewares/middlewares');
+const { validateToken, isAdmin } = require('../middlewares/middlewares');
 const multer = require("multer");
 const usersRouter = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
@@ -11,10 +11,12 @@ usersRouter.post('/login', userController.userLogin);
 usersRouter.get('/user', validateToken, userController.getUser);
 usersRouter.get('/users', userController.getUsers);
 usersRouter.put('/update-preferences', validateToken, userController.updateUserPreferences);
+usersRouter.patch('/user', validateToken, userController.patchUser);
 usersRouter.put('/forgotpassword', userController.forgotPassword);
 usersRouter.post('/upload', validateToken, upload.single('avatar'), userController.setAvatar);
 usersRouter.get('/userbyid/:userId',validateToken, userController.getUserById);
 usersRouter.post('/subscription', userController.emailSubscribe );
-usersRouter.delete('/delete',validateToken, userController.deleteUser );
+usersRouter.delete('/profile',validateToken, userController.deleteUser );
+usersRouter.delete("/user/:userId", validateToken, userController.deleteUserByAdmin);
 
 module.exports = usersRouter;
