ea569e83203b6e019688f3466444d4eceb5633d0
Luigi Cucciolillo <luigi.cucciolillo@gmail.com>
2025-03-12 08:03:10 +0100

    Stable CRUD operations on new entity of events. documentation in extra files

--- models/Event_v2.js
+++ models/Event_v2.js
@@ -3,15 +3,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const EventV2Schema = new Schema({
   title: { type: String, required: true },
-  city: { type: mongoose.Schema.Types.ObjectId, ref: 'City' },
+  city: { type: String },//{ type: mongoose.Schema.Types.ObjectId, ref: 'City' },
   description: { type: String, required: true },
   administrator: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   published: { type: String },
   confirmed: { type: String },
+  budget: { type: String },
+  dateAndTime: { type: String },
   closed: { type: String },
-  dateTime: { type: Date, required: true },
-  location: { type: mongoose.Schema.Types.ObjectId, ref: 'Location' },
-  category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
+  dateTime: { type: Date},
+  location: { type: String },// { type: mongoose.Schema.Types.ObjectId, ref: 'Location' },
+  location_str: { type: String },
+  category: { type: String},//{ type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
   photos: [{ type: String }] // array photos (url, path...)
 }, { collection: 'Events_v2' });
 module.exports = mongoose.model("Events_v2", EventV2Schema);
